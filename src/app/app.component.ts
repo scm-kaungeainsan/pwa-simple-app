11bc9fe0811ef8f82dc94bdeb880348cc1469a1d
scm-kaungeainsan <scm.dott.developer5@gmail.com>
2022-02-21 02:24:34 +0630

    push docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pwa-simple-app';
+  constructor(public updates:SwUpdate) {
+    updates.available.subscribe(event => {
+      updates.activateUpdate().then(() => this.updateApp());
+  });
+  }
+
+  updateApp(){
+    document.location.reload();
+    console.log("The app is updating right now");
+
+   }
 }
